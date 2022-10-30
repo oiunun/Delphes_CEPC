d98f4c45329faf47952d3ee1a4d6c8caea446878
1ouhu <a1261926744@163.com>
2022-10-30 10:38:29 +0800

    change

--- job/job_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /cvmfs/sft.cern.ch/lcg/views/LCG_99/x86_64-centos7-gcc10-opt/setup.sh
-cd /cefs/higgs/gaoxu/delphes
-./DelphesSTDHEP cards/delphes_card_CEPC_4th.tcl /cefs/higgs/gaoxu/delphes/rootfile/e2e2h_X_1_99_1.root /cefs/data/stdhep/CEPC240/higgs/update_from_LiangHao_1M/data/E240.Pe2e2h_X.e0.p0.whizard195/e2e2h_X.e0.p0.000*.stdhep
